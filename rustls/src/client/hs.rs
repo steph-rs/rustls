09e86f6a7079ac9a73e3d755772749a611ae9798
Steph Sinyakov <steph.sinyakov@gmail.com>
2025-02-15 17:48:30 +0100

    debug

--- rustls/src/client/hs.rs
+++ rustls/src/client/hs.rs
@@ -651,7 +651,7 @@ impl State<ClientConnectionData> for ExpectServerHello {
         //     }
         // }
 
-        let sgx_suite = CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256;
+        let sgx_suite = CipherSuite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256;
         let suite = config
             .find_cipher_suite(sgx_suite)
             .ok_or_else(|| {
