81fb05c153912f54b8cd045c49a95d3e0f260b09
Steph Sinyakov <steph.sinyakov@gmail.com>
2025-02-15 17:40:52 +0100

    debug

--- rustls/src/client/hs.rs
+++ rustls/src/client/hs.rs
@@ -644,14 +644,14 @@ impl State<ClientConnectionData> for ExpectServerHello {
 
         println!("chiper suite: {:?}", server_hello.cipher_suite);
 
-        let all_suites = &config.provider.cipher_suites;
-        for suite in all_suites {
-            if !core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::SHA256) {
-                println!("Found non-SHA256 suite: {:?}", suite.suite());
-            }
-        }
+        // let all_suites = &config.provider.cipher_suites;
+        // for suite in all_suites {
+        //     if !core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::SHA256) {
+        //         println!("Found non-SHA256 suite: {:?}", suite.suite());
+        //     }
+        // }
 
-        let sgx_suite = CipherSuite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256;
+        let sgx_suite = CipherSuite::TLS13_AES_256_GCM_SHA384;
         let suite = config
             .find_cipher_suite(sgx_suite)
             .ok_or_else(|| {
