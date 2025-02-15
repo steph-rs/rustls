96c17b597853f1e7d461dde31bfe623b6cc56ca8
Steph Sinyakov <steph.sinyakov@gmail.com>
2025-02-15 17:53:59 +0100

    debug

--- rustls/src/client/hs.rs
+++ rustls/src/client/hs.rs
@@ -644,14 +644,23 @@ impl State<ClientConnectionData> for ExpectServerHello {
 
         println!("chiper suite: {:?}", server_hello.cipher_suite);
 
-        // let all_suites = &config.provider.cipher_suites;
-        // for suite in all_suites {
-        //     if !core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::SHA256) {
-        //         println!("Found non-SHA256 suite: {:?}", suite.suite());
-        //     }
-        // }
+        let all_suites = &config.provider.cipher_suites;
+        for suite in all_suites {
+            if core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::MD5) {
+                println!("Found MD5 suite: {:?}", suite.suite());
+            }
+            if core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::SHA1) {
+                println!("Found SHA1 suite: {:?}", suite.suite());
+            }
+            if core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::SHA224) {
+                println!("Found SHA224 suite: {:?}", suite.suite());
+            }
+            if core::ptr::eq(&suite.hash_provider().algorithm(), &crypto::hash::HashAlgorithm::SHA512) {
+                println!("Found SHA512 suite: {:?}", suite.suite());
+            }
+        }
 
-        let sgx_suite = CipherSuite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256;
+        let sgx_suite = CipherSuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256;
         let suite = config
             .find_cipher_suite(sgx_suite)
             .ok_or_else(|| {
